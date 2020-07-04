f42a2f87c11ff7288d9772ef70b83ec166af051e
Sonia <soniawadji@gmail.com>
2020-07-05 01:22:58 +0530

    Initial Commit

---
+++ app/src/main/java/com/hangrycoder/neumorphiccalculator/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.hangrycoder.neumorphiccalculator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.ui.core.setContent
+import androidx.ui.foundation.Text
+import androidx.ui.tooling.preview.Preview
+import com.hangrycoder.neumorphiccalculator.ui.NeumorphicCalculatorTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp()
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    NeumorphicCalculatorTheme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyApp()
+}
