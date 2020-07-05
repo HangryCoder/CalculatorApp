c086900599924e7cbb4bff6fb76dd6b19b10e95c
Sonia <soniawadji@gmail.com>
2020-07-05 19:57:18 +0530

    Using vector image for input background. Looks great!

--- app/src/main/java/com/hangrycoder/neumorphiccalculator/MainActivity.kt
+++ app/src/main/java/com/hangrycoder/neumorphiccalculator/MainActivity.kt
@@ -4,19 +4,21 @@ import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.ui.core.ContentScale
 import androidx.ui.core.Modifier
+import androidx.ui.core.clip
 import androidx.ui.core.setContent
 import androidx.ui.foundation.Image
 import androidx.ui.foundation.Text
-import androidx.ui.layout.Column
+import androidx.ui.foundation.shape.corner.RoundedCornerShape
+import androidx.ui.layout.*
 import androidx.ui.layout.ColumnScope.weight
-import androidx.ui.layout.padding
 import androidx.ui.material.*
 import androidx.ui.res.imageResource
+import androidx.ui.res.vectorResource
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
 import com.hangrycoder.neumorphiccalculator.ui.NeumorphicCalculatorTheme
-import com.hangrycoder.neumorphiccalculator.ui.darkBlue
 import com.hangrycoder.neumorphiccalculator.ui.darkGrey
 import com.hangrycoder.neumorphiccalculator.ui.lightGrey
 
@@ -60,9 +62,16 @@ fun Toolbar() {
 
 @Composable
 fun DigitalInputContainer(resId: Int) {
-    val image = imageResource(resId)
+    val imageModifier = Modifier
+            .padding(16.dp)
+            .preferredHeight(150.dp)
+            .fillMaxWidth()
+            .clip(shape = RoundedCornerShape(8.dp))
+
+    val image = vectorResource(resId)
     Image(image,
-            modifier = Modifier.padding(16.dp, 0.dp))
+            modifier = imageModifier,
+            contentScale = ContentScale.Crop)
 }
 
 @Preview(showBackground = true)
