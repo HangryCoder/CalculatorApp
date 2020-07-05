12e2dd49e160b6aa51cac681b7d85204d7a6217d
Sonia <soniawadji@gmail.com>
2020-07-05 18:49:56 +0530

    Added digital input container image. Need to set it correctly.

--- app/src/main/java/com/hangrycoder/neumorphiccalculator/MainActivity.kt
+++ app/src/main/java/com/hangrycoder/neumorphiccalculator/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.hangrycoder.neumorphiccalculator
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.Modifier
 import androidx.ui.core.setContent
+import androidx.ui.foundation.Image
 import androidx.ui.foundation.Text
 import androidx.ui.layout.Column
 import androidx.ui.layout.ColumnScope.weight
 import androidx.ui.layout.padding
 import androidx.ui.material.*
+import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
 import com.hangrycoder.neumorphiccalculator.ui.NeumorphicCalculatorTheme
@@ -38,12 +41,24 @@ fun MyApp(content: @Composable() () -> Unit) {
 @Composable
 fun MyScreenContent() {
     Column(modifier = Modifier.weight(1f)) {
-        TopAppBar(title = {
-            Text(text = "Calculator",
-                    color = lightGrey,
-                    style = MaterialTheme.typography.h4)
-        }, backgroundColor = darkGrey)
+        Toolbar()
+        DigitalInputContainer(resId = R.drawable.ic_digital_input_container)
     }
+}
+
+@Composable
+fun Toolbar() {
+    TopAppBar(title = {
+        Text(text = "Calculator",
+                color = lightGrey,
+                style = MaterialTheme.typography.h4)
+    }, backgroundColor = darkGrey)
+}
+
+@Composable
+fun DigitalInputContainer(resId: Int) {
+    val image = imageResource(resId)
+    Image(image)
 }
 
 @Composable
